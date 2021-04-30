935ce2d87fce3e5a8ee2e4f7e6bcbbcbe79ec121
Danilo Guanabara <danilo@sensorial.systems>
2021-04-30 13:11:45 -0300

    Updating initialization mechanism

--- web-revu/web-revu.js
+++ web-revu/web-revu.js
@@ -1,13 +1,14 @@
 import WebComponent from "/web-component/web-component.js"
 
 let endpoint = "https://f4do7hzoab.execute-api.sa-east-1.amazonaws.com/default/ec-management"
+let domain = "br.ps.revu.design";
 let expectedLoadingTime = 20000; // 20 seconds.
 
 export default class WebRevu extends WebComponent {
     onStarted() {
         let iframe = this.root.getElementById("iframe");
         let loading = this.root.getElementById("loading");
-        iframe.src = "https://br.ps.revu.design";
+        iframe.src = `https://${domain}`;
         iframe.classList.remove("hidden");
         loading.classList.add("hidden");
     }
@@ -23,9 +24,28 @@ export default class WebRevu extends WebComponent {
         console.log(`Response: ${text}`);
     }
 
+    startInstance() {
+        this.exec("Start");
+        this.ws = new WebSocket(`wss://${domain}`);
+        this.ws.onopen = (function(revu) {
+            return function() {
+                revu.ws.close();
+                revu.ws = null;
+                revu.onStarted();
+            }
+        })(this);
+
+        this.ws.onerror = (function(revu) {
+            return function() {
+                revu.startInstance(); // We try again.
+            }
+        })(this);
+    }
+
     async onload(root) {
         this.root = root;
-        this.exec("Start");
+        this.startInstance();
+
         setTimeout((function(revu) {
             return function() {
                 revu.onStarted()
